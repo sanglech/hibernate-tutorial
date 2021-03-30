29c24a24d2a271e4903ac02645ba198765882dc5
Christian <christian@sanglech.com>
2021-03-31 00:35:28 +0900

    Primary Key Demo

--- src/com/christian/hibernate/demo/PrimaryKeyDemo.java
+++ src/com/christian/hibernate/demo/PrimaryKeyDemo.java
@@ -6,10 +6,9 @@ import org.hibernate.cfg.Configuration;
 
 import com.christian.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class PrimaryKeyDemo {
 
 	public static void main(String[] args) {
-		
 		///create session factory
 		SessionFactory factory = new Configuration()
 									.configure("hibernate.cfg.xml")
@@ -17,24 +16,23 @@ public class CreateStudentDemo {
 									.buildSessionFactory();
 		
 		//create session
-		
 		Session session= factory.getCurrentSession();
 		
 		try {
 			
-			//create a student obj
-			
-			System.out.println("Creating a new Student obj");
-			
-			Student tempStudent = new Student("Christian","Sangle","christian@sangle.com");
+			//create multiple studen objects
+			Student studentOne = new Student("Mary","Poppins","mary@poppins.com");
+			Student studentTwo = new Student("John","Doe","john@doe.com");
+			Student studentThree = new Student("Apple","Bom","apple@bom.com");
 			
 			//start txn
 			session.beginTransaction();
 			
-			//save the student obj
+			//save the student objs
 			System.out.println("Saving new Student obj");
-			session.save(tempStudent);
-			
+			session.save(studentOne);
+			session.save(studentTwo);
+			session.save(studentThree);
 			
 			//comit txn
 			session.getTransaction().commit();
